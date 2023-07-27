3357c7e36d8daf80932725254f42a8e8fde05f59
Bharathi-raja-pbr <76565518+Bharathi-raja-pbr@users.noreply.github.com>
2023-07-27 15:02:55 +0530

    Create Insert  a Node at the Tail of a Linked List.py

---
+++ Linked Lists/Insert  a Node at the Tail of a Linked List.py
@@ -0,0 +1,100 @@
+'''
+You are given the pointer to the head node of a linked list and an integer to add to the list. Create a new node with the given integer. Insert this node at the tail of the linked list and return the head node of the linked list formed after inserting this new node. The given head pointer may be null, meaning that the initial list is empty.
+
+Function Description
+
+Complete the insertNodeAtTail function in the editor below.
+
+insertNodeAtTail has the following parameters:
+
+SinglyLinkedListNode pointer head: a reference to the head of a list
+int data: the data value for the node to insert
+
+Returns
+
+SinglyLinkedListNode pointer: reference to the head of the modified linked list
+
+Input Format
+
+The first line contains an integer n , the number of elements in the linked list.
+The next n lines contain an integer each, the value that needs to be inserted at tail.
+
+Constraints
+0<=n,listi <=1000
+
+Sample Input
+
+STDIN Function ----- -------- 5 size of linked list n = 5 141 linked list data values 141..474 302 164 530 474
+
+Sample Output
+
+141
+302
+164
+530
+474
+'''
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+
+def print_singly_linked_list(node, sep, fptr):
+    while node:
+        fptr.write(str(node.data))
+
+        node = node.next
+
+        if node:
+            fptr.write(sep)
+
+# Complete the insertNodeAtTail function below.
+
+#
+# For your reference:
+#
+# SinglyLinkedListNode:
+#     int data
+#     SinglyLinkedListNode next
+#
+#
+def insertNodeAtTail(head, data):
+    if head is None:
+        head=SinglyLinkedListNode(data)
+        return head
+    n=head
+    while n.next!=None:
+        n=n.next
+    n.next=SinglyLinkedListNode(data)
+    
+    return head
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    llist_count = int(input())
+
+    llist = SinglyLinkedList()
+
+    for i in range(llist_count):
+        llist_item = int(input())
+        llist_head = insertNodeAtTail(llist.head, llist_item)
+        llist.head = llist_head
+
+    print_singly_linked_list(llist.head, '\n', fptr)
+    fptr.write('\n')
+
+    fptr.close()
